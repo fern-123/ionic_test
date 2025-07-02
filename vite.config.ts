6aa5da96722d3111e2b9e33010537393849edfb8
me <me@me.me>
2025-07-02 19:27:26 -0400

    corrected vite.config.ts syntax error

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,9 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/ionic_test/',
   plugins: [
     react(),
-    base: '/inonic_test/',
     legacy()
   ],
   test: {
